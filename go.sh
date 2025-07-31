c70143555739b56329cc2080e0c2804181b970d6
Christian Mesh <christianmesh1@gmail.com>
2025-07-31 15:09:14 -0400

    Add workflow

--- go.sh
+++ go.sh
@@ -6,11 +6,5 @@ done
 export TF_PLUGIN_CACHE_DIR=$PWD/cache/
 rm -r "$TF_PLUGIN_CACHE_DIR"
 
-# Download the installer script:
-curl --proto '=https' --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh
-# Give it execution permissions:
-chmod +x install-opentofu.sh
-# Run the installer:
-sudo ./install-opentofu.sh --install-method deb
 
 ls | grep job | xargs -P 20 -I foo bash -c "cd foo; tofu init"
